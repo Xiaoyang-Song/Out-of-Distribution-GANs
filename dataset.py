89cc9d7148d81f8cd3a25786da2c30d05fd109b1
xysong <xysong25@gmail.com>
2022-06-09 01:34:45 -0400

    Set up MNIST_SUB functions

--- dataset.py
+++ dataset.py
@@ -17,6 +17,27 @@ def MNIST(batch_size, test_batch_size, num_workers=0, shuffle=True):
     return train_set, val_set, train_loader, test_loader
 
 
+def MNIST_SUB(batch_size: int, val_batch_size: int, idx_ind: list, idx_ood: list, shuffle=True):
+    """
+    Helper function to extract subset of the MNIST dataset. In short, return training 
+    and validation sets with labels specified in 'idx_ind' and 'idx_ood', respectively. For 
+    samples with other labels, just ignore them.
+
+    Args:
+        batch_size (int): training batch size.
+        val_batch_size (int): validation batch size.
+        idx_ind (list): a list of integer from 0-9 (specifying in-distribution labels)
+        idx_ood (list): a list of integer from 0-9 (specifying out-of-distribution labels)
+        shuffle (bool, optional): whether or not to shuffle the dataset. Defaults to True.
+    """
+    train_set = torchvision.datasets.MNIST(
+        "./Datasets", download=True, transform=transforms.Compose([transforms.ToTensor()]))
+    val_set = torchvision.datasets.MNIST(
+        "./Datasets", download=True, train=False, transform=transforms.Compose([transforms.ToTensor()]))
+    return train_set, val_set
+    # pass
+
+
 def CIFAR10(batch_size, test_batch_size):
     transform = transforms.Compose(
         [transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
@@ -37,22 +58,34 @@ def CIFAR10(batch_size, test_batch_size):
 
 if __name__ == '__main__':
     # Test dataset functions
-    train_dataset, val_dataset, train_loader_mnist, val_loader_mnist = MNIST(
-        32, 16)
-    # Test single sample:
-    ic(len(train_dataset.__getitem__(0)))  # First sample in the loader
-    ic(train_dataset.__getitem__(0)[0].shape)  # img features
-    ic(train_dataset.__getitem__(0)[1])  # Class label
-    mnist_sample = train_dataset.__getitem__(0)[0]
-    # Show the sample image
-    # plt.imshow(mnist_sample.squeeze(), cmap="gray")
+    # train_dataset, val_dataset, train_loader_mnist, val_loader_mnist = MNIST(
+    #     32, 16)
+    # # Test single sample:
+    # ic(len(train_dataset.__getitem__(0)))  # First sample in the loader
+    # ic(train_dataset.__getitem__(0)[0].shape)  # img features
+    # ic(train_dataset.__getitem__(0)[1])  # Class label
+    # mnist_sample = train_dataset.__getitem__(0)[0]
+    # # Show the sample image
+    # # plt.imshow(mnist_sample.squeeze(), cmap="gray")
+    # # plt.show()
+    # # ic(mnist_sample)
+
+    # VISUALIZE CIFAR-10 dataset
+    # train_dataset, val_dataset, train_loader_mnist, val_loader_mnist = CIFAR10(
+    #     32, 16)
+    # ic(train_dataset.__getitem__(0)[0].shape)  # img features
+    # cifar_sample_grayscale = train_dataset.__getitem__(7)[0].mean(0)
+    # cifar_sample_label = train_dataset.__getitem__(7)[1]
+    # ic(cifar_sample_label)  # By manually inspection, this should be a horse
+    # plt.imshow(cifar_sample_grayscale.squeeze(), cmap="gray")
     # plt.show()
-    # ic(mnist_sample)
-    train_dataset, val_dataset, train_loader_mnist, val_loader_mnist = CIFAR10(
-        32, 16)
-    ic(train_dataset.__getitem__(0)[0].shape)  # img features
-    cifar_sample_grayscale = train_dataset.__getitem__(7)[0].mean(0)
-    cifar_sample_label = train_dataset.__getitem__(7)[1]
-    ic(cifar_sample_label)  # By manually inspection, this should be a horse
-    plt.imshow(cifar_sample_grayscale.squeeze(), cmap="gray")
-    plt.show()
+
+    # MNIST_SUB Sanity Check
+    train_set, val_set = MNIST_SUB(
+        128, 64, idx_ind=[0, 2, 3, 6, 8], idx_ood=[1, 7])
+    ic(train_set.targets.shape)
+    ic(train_set.data.shape)
+    ic(type(train_set.data))
+    train_loader = torch.utils.data.DataLoader(
+        train_set.data, batch_size=128, shuffle=True)
+    ic(train_loader)
