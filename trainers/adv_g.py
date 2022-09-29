3835890d4bfaee5581a6cc4be21a5c2c44db5dde
xysong <xysong25@gmail.com>
2022-09-29 00:58:28 -0400

    code cleaning

--- trainers/adv_g.py
+++ trainers/adv_g.py
@@ -8,18 +8,16 @@ from wasserstein import Wasserstein
 
 
 def grad_asc_w_rej(ind_loader, D, B, M, Wt, WLoss, device=DEVICE):
-    # D.requires_grad = False
     G_x = []
     for i, (x, y) in enumerate(ind_loader):
-
         for img in x:
             p = img.clone().unsqueeze(0).to(device)
-            p.requires_grad_()
-            # ic(p.shape)
+            p.requires_grad_()  # Enforce requires grad
             g_x = ad_atk(D, p, Wt, WLoss)
             G_x.append(g_x)
 
         if i >= M:
+            ic(f"{B * M} adversarial samples are generated.")
             return G_x
 
 
@@ -27,32 +25,25 @@ def ad_atk(D, x, Wt, WLoss):
     assert x.requires_grad
     ic(x.shape)
     x = nn.Parameter(x)
-    optimizer = torch.optim.Adam(
-        [{"params": x}], lr=1e-3)
-
-    lr = 1
+    optimizer = torch.optim.Adam([{"params": x}], lr=1e-3)
     i = 1
     while True:
         # Aim to minimize W
         optimizer.zero_grad()
         logit = D(x)
-        # ic(logit.shape)
-        # W = neg_zero_softmax_loss(logit)
-        W = WLoss(torch.softmax(logit, dim=-1))
-        ic(W)
+        # Minimize negative Wasserstein distances to onehot vectors
+        W = -WLoss(torch.softmax(logit, dim=-1))
+        # ic(W)
         if W < Wt:
             print(f"Adversarial Attack done in {i} iterations")
             return x.detach()
-        # x.retain_grad()
         W.backward()
         optimizer.step()
-        # grad = x.grad.data
-        # ic(grad.shape)
-        # # Update
-        # dx = lr * grad / grad.norm()  # Normalize
-        # x.data -= lr * dx  # Can still do Gradient Dscent
-        # x.grad.zero_()
         i += 1
+
+
+def to_raw(W_t):
+    return -math.exp(-W_t)
 
 
 def find_W_t():
@@ -69,23 +60,24 @@ if __name__ == '__main__':
     pretrain = torch.load(path)
     D.load_state_dict(pretrain['model_state_dict'])
     print("Pretrained D state is loaded.")
-    # D.parameters().retain_grad()
-    # ic(D[0].weight.is_leaf)
 
     # Load dataset
     idx_ind = [0, 1, 3, 4, 5]
     dset_dict = MNIST_SUB(batch_size=2, val_batch_size=64,
                           idx_ind=idx_ind, idx_ood=[2], shuffle=True)
     ind_tri_loader = dset_dict['train_set_ind_loader']
-    # batch = next(iter(ind_tri_loader))[0]
+
     # Start Gradient Ascent
     WLoss = Wasserstein.apply
-    G_x = grad_asc_w_rej(ind_tri_loader, D, 3, 1, -0.25, WLoss, device=DEVICE)
+    nl_Wt = 1.50  # 0.08 - 0.80
+    Wt = to_raw(nl_Wt)
+    G_x = grad_asc_w_rej(ind_tri_loader, D, 3, 1,
+                         Wt, WLoss, device=DEVICE)
     for img in G_x:
         ic(img.shape)
         ic(WLoss(torch.softmax(D(img), dim=-1)))
-        ic((-WLoss(torch.softmax(D(img), dim=-1))).log())
+        ic(-(WLoss(torch.softmax(D(img), dim=-1)).log()))
     img = torch.cat(G_x)
     ic(img.shape)
-    show_images(img)
-    plt.show()
+    # show_images(img)
+    # plt.show()
