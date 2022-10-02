4690526bf9bb98e783dcdbc69e5b82af48f16a9f
xysong <xysong25@gmail.com>
2022-10-01 21:02:02 -0400

    Visualization Sync

--- Visualization/wloss_visualization.py
+++ Visualization/wloss_visualization.py
@@ -1,30 +1,38 @@
 from config import *
 from wasserstein import *
 from trainers.detector_trainer import *
+from models.dc_gan_model import dc_discriminator, GAN_TYPE
 
 
 def img_to_wloss(D, img):
     assert type(img) == torch.Tensor or type(
         img) == list, 'Expect input to be either a torch tensor or a list of tuples'
-    if type(img) == torch.Tensor:
-        w_loss = np.array([ood_wass_loss(D(x)).item() for x in img])
-    else:
-        w_loss = [ood_wass_loss(D(x[0])) for x in img]
-    return np.array(w_loss)
+    if type(img) != torch.Tensor:
+        img = torch.stack([x[0] for x in img], dim=0)
+    # ic(D(img[0].unsqueeze(0)))
+    w_loss = np.array(ood_wass_loss(F.softmax(D(img), dim=-1)))
+    # ic(w_loss)
+    return w_loss
 
 
 def raw_to_nll(w_loss):
+    # Filter out invalid wass_loss (floating point error)
+    w_loss = w_loss[w_loss > 0]
     return -np.log(w_loss)
 
 
 def plot_w_loss(w_loss: list, color_key: list, path: str):
     # w_loss is a list of list
     assert len(w_loss) == len(color_key)
+    label = ['InD', 'OoD']
     for idx, wass in enumerate(w_loss):
         num_pts = len(wass)
         noise_y = np.random.normal(0, 1, num_pts)
-        plt.scatter(wass, noise_y, c=color_key[idx])
+        plt.scatter(wass, noise_y,
+                    c=color_key[idx], label=label[idx], alpha=0.4)
+    plt.legend()
     plt.savefig(path)
+    plt.close()
 
 
 if __name__ == '__main__':
@@ -32,15 +40,21 @@ if __name__ == '__main__':
     idx_ind = [0, 1, 3, 4, 5]
     dset_dict = MNIST_SUB(batch_size=2, val_batch_size=64,
                           idx_ind=idx_ind, idx_ood=[2], shuffle=True)
-    tri_set = dset_dict['train_set_ind'][0:100]
-    ood_set = dset_dict['train_set_ood'][0:100]
+    tri_set = dset_dict['train_set_ind']
+    ood_set = dset_dict['train_set_ood']
 
-    D = dc_discriminator(GAN_TYPE.OOD).to(DEVICE)
-    D.load_state_dict(torch.load("checkpoint/adv_pre_D.pt"))
+    img_info = {'H': 28, 'W': 28, 'C': 1}
+    # Load pretrained discriminator
+    D = dc_discriminator(img_info, GAN_TYPE.OOD).to(DEVICE)
+    path = 'checkpoint/adv_pre_D.pt'
+    pretrain = torch.load(path)
+    D.load_state_dict(pretrain['model_state_dict'])
     print("Pretrained D state is loaded.")
 
     g_img = torch.load("checkpoint/adv_g_img(cpu).pt")
     # Test visualization functionality
     color = ['#ffbf00', '#00b384']
-    w_pts = [img_to_wloss(D, dset) for dset in (tri_set, ood_set)]
-    plot_w_loss(w_pts, color, "Visualization/w_vis.png")
+    w_pts = [raw_to_nll(img_to_wloss(D, dset)) for dset in (tri_set, ood_set)]
+    plot_w_loss(w_pts, color, "Visualization/w_vis_ood_ind.png")
+    w_pts = [raw_to_nll(img_to_wloss(D, dset)) for dset in (tri_set, g_img)]
+    plot_w_loss(w_pts, color, "Visualization/w_vis_g_ind.png")
