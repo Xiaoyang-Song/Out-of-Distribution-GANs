db455b89b284e2cff7a8f4cca2ac4a3c75ff1900
xysong <xysong25@gmail.com>
2022-10-06 01:51:14 -0400

    Test functionality of UMAP

--- sobol_seq.py
+++ sobol_seq.py
@@ -1,6 +1,7 @@
 from config import *
 from Visualization.umap_plot import umap_visualization
 from scipy.stats import qmc
+from dataset import MNIST
 
 
 def sobol_seq(d: int, m: int, scramble: bool):
@@ -12,4 +13,16 @@ def sobol_seq(d: int, m: int, scramble: bool):
 if __name__ == '__main__':
     ic("Hello sobol_seq.py")
     sobol_img = sobol_seq(d=784, m=10, scramble=False)
-    umap_visualization(sobol_img, torch.ones(2**10))
+    sobol_y = torch.zeros(sobol_img.shape[0])
+    # umap_visualization(sobol_img, torch.ones(2**10))
+
+    mnist_tri_set, mnist_val_set, mnist_tri_loader, mnist_val_loader = MNIST(
+        128, 64, 2, True)
+    mnist_tri_img = torch.stack([x[0] for x in mnist_tri_set]).flatten(1, 3)
+    mnist_tri_label = torch.ones(mnist_tri_img.shape[0])
+    ic(mnist_tri_img.shape)
+    x_vis = torch.cat([mnist_tri_img, sobol_img])
+    y_vis = torch.cat([mnist_tri_label, sobol_y])
+    ic(x_vis.shape)
+    ic(y_vis.shape)
+    # umap_visualization(torch.cat())
