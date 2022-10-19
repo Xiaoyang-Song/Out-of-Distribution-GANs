a78ab3a53e4ff33d464ee894685bdf676595b7af
xysong <xysong25@gmail.com>
2022-10-19 00:35:17 -0400

    knn search done

--- models/sobol.py
+++ models/sobol.py
@@ -8,8 +8,15 @@ def knn_search(Decoder, ood_feat_vec, k, d, m):
     assert ood_feat_vec.shape[1] == d
     # sobol_feat: 2^m x d
     sobol_feat = sobol_seq(d=d, m=m, scramble=False)
-    # Matrix Broadcasting
-    
+    # Matrix Broadcasting: B x 2^m
+    dist = (ood_feat_vec - sobol_feat.T).norm(dim=1)
+    # idx: B x k
+    vals, idx = torch.topk(dist, dim=1, k=k, largest=False)
+    idx = idx.squeeze()
+    # Bk x d
+    feat_vec = torch.cat([sobol_feat[i.squeeze()] for i in idx]).squeeze()
+    # Generate samples
+    return feat_vec, Decoder(feat_vec)
 
 
 if __name__ == '__main__':
