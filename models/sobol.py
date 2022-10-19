5c4712206f24ab6327d13286655526b60261544d
xysong <xysong25@gmail.com>
2022-10-19 00:21:07 -0400

    KNN broadcasting exp

--- models/sobol.py
+++ models/sobol.py
@@ -2,13 +2,15 @@ from config import *
 from sobol_seq import sobol_seq
 
 
-def knn_search(Decoder, ood_feat_vec, k, d, num_grids):
+def knn_search(Decoder, ood_feat_vec, k, d, m):
     # ood_feat_vec: B x d
     assert len(ood_feat_vec.shape) == 2
     assert ood_feat_vec.shape[1] == d
-
-    sobol_feat = sobol_seq(d=d, m=num_grids, scramble=False)
+    # sobol_feat: 2^m x d
+    sobol_feat = sobol_seq(d=d, m=m, scramble=False)
+    # Matrix Broadcasting
     
+
 
 if __name__ == '__main__':
     ic('Sobol sequence generation')
