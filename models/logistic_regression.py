9b967b13c323e039aebf316a7defa884c2e492d5
xysong <xysong25@gmail.com>
2022-10-27 14:00:46 -0400

    setup logistic regression code

--- models/logistic_regression.py
+++ models/logistic_regression.py
@@ -1,4 +1,11 @@
 from config import *
+from sklearn.linear_model import LogisticRegression
+
+
+def fit_log_reg(X, y, random_state=0):
+    clf = LogisticRegression(random_state=random_state).fit(X, y)
+    ic(clf.score(X, y))
+    return clf
 
 
 if __name__ == '__main__':
