a45b2dd365ea7246a4ac7f0073b0ef262dac6c7b
xysong <xysong25@gmail.com>
2022-10-29 21:24:41 -0400

    add helper function

--- models/logistic_regression.py
+++ models/logistic_regression.py
@@ -1,11 +1,17 @@
 from config import *
 from sklearn.linear_model import LogisticRegression
+from wass_loss import *
+from wasserstein import *
 
 
 def fit_log_reg(X, y, random_state=0):
     clf = LogisticRegression(random_state=random_state).fit(X, y)
     ic(clf.score(X, y))
     return clf
+
+
+def dset_to_x_y(dset, n):
+    return torch.stack([x[0] for x in dset])[0:n], torch.tensor([x[1] for x in dset])[0:n]
 
 
 if __name__ == '__main__':
