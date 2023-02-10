665967046629b9ff4e83ea3cdc4ac2b01e5cea88
xysong <xysong25@gmail.com>
2023-02-10 08:39:47 -0500

    Sync

--- models/boundary.py
+++ models/boundary.py
@@ -5,24 +5,22 @@ from dataset import *
 def nn_search(xin, xout, k):
     xin, yin = tuple_list_to_tensor(xin)
     x, y = xin, yin
-    xout, yout = tuple_list_to_tensor(xout)
-    ic(f"{xout.shape}; {yout.shape}")
     # Find K Nearest Neighbor
     xin = xin.reshape((-1, 784, 1))
     xout = xout.reshape((-1, 784, 1))
-    ic(f"{xin.shape}; {xout.shape}; {xin.permute((2, 1, 0)).shape}")
+    # ic(f"{xin.shape}; {xout.shape}; {xin.permute((2, 1, 0)).shape}")
     diff = xout - xin.permute((2, 1, 0))
-    ic(diff.shape)
+    # ic(diff.shape)
     # Compute norm
     dist = torch.norm(diff, dim=1)
     ic(dist.shape)
     # K NN
     vals, idx = torch.topk(dist, dim=1, k=k, largest=False)
     idx = idx.squeeze()
-    ic(idx.shape)
+    # ic(idx.shape)
     # Flatten and remove duplicate
     idx = idx.reshape((1, -1)).squeeze().unique()
-    ic(idx.shape)
+    # ic(idx.shape)
     # Get results
     x, y = x[idx, :, :, :], y[idx]
     ic(f"{x.shape}; {y.shape}")
@@ -37,6 +35,7 @@ if __name__ == '__main__':
     mnist = MNIST_By_CLASS()
     xin, xout = form_ind_dsets(mnist, ind), form_ind_dsets(mnist, ood)
     ic(f"xin: {len(xin)}; xout: {len(xout)}")
+    xout, yout = tuple_list_to_tensor(xout)
     nn_search(xin, xout, 10)
     # Cast to tensors for KNN search
     # xin, yin = tuple_list_to_tensor(xin)
--- models/logistic_regression.py
+++ models/logistic_regression.py
@@ -15,16 +15,16 @@ def dset_to_x_y(dset, n):
 
 
 def plot_lr_result(x, y_m, y_fm):
-    plt.plot(x, y_m, marker='o', label="MNIST")
-    plt.plot(x, y_fm, marker='s', label="FashionMNIST")
-    plt.plot(x, (y_m + y_fm) / 2, marker='x', label="Total")
+    plt.plot(x, y_m, marker='o', label="InD")
+    plt.plot(x, y_fm, marker='s', label="OoD")
+    # plt.plot(x, (y_m + y_fm) / 2, marker='x', label="Total")
     plt.legend()
     # plt.xlim(xmin=10)
     # plt.ylim(ymin=0.1, ymax=0.7)
     plt.xlabel("Number of OoD Samples Seen")
     plt.xticks(x, x)
     plt.title("Detection Accuracy vs. Number of OoD Samples")
-    plt.savefig("checkpoint/MNIST-FashionMNIST/lr.png")
+    plt.savefig("checkpoint/MNIST/lr.png")
     plt.close()
 
 
@@ -80,7 +80,37 @@ def plot_lr_result(x, y_m, y_fm):
 
 if __name__ == '__main__':
     ic("Logistic Regression")
-    n = np.array([8, 16, 32, 64, 128, 256])
-    y_m = np.array([0.96733, 0.9743, 0.9766, 0.967, 0.947, 0.9306])
-    y_fm = np.array([0.93556, 0.9864, 0.9826, 0.9926, 0.9953, 0.9999])
+    n = np.array([8, 16, 32, 64, 128, 256, 512])
+    # y_m = np.array([0.96733, 0.9743, 0.9766, 0.967, 0.947, 0.9306])
+    # y_fm = np.array([0.93556, 0.9864, 0.9826, 0.9926, 0.9953, 0.9999])
+    # 5-feat: mnist
+    # y_m = np.array([0.9523, 0.9677, 0.9773, 0.98, 0.984, 0.987, 0.988])
+    # y_fm = np.array([0.753, 0.854, 0.964, 0.972,0.9923, 0.995, 0.995])
+    # plot_lr_result(n, y_m, y_fm)
+
+    # 128-feat:mnist
+    # y_m = np.array([0.9543, 0.962, 0.974, 0.976, 0.9803, 0.9796, 0.9865])
+    # y_fm = np.array([0.773, 0.8712, 0.9581, 0.9645, 0.9826, 0.9854, 0.9855])
+    # plot_lr_result(n, y_m, y_fm)
+
+    # PCA: mnist
+    # y_m = np.array([0.940, 0.952, 0.9568, 0.965, 0.9698, 0.9702, 0.9814])
+    # y_fm = np.array([0.762, 0.884, 0.956, 0.9637, 0.9740, 0.9753, 0.9812])
+    # plot_lr_result(n, y_m, y_fm)
+
+    # fashionmnist: 5-feat
+    # y_m = np.array([0.923, 0.932, 0.954, 0.96, 0.9685, 0.9732, 0.974])
+    # y_fm = np.array([0.653, 0.821, 0.9481, 0.971, 0.975, 0.9794, 0.983])
+    # plot_lr_result(n, y_m, y_fm)
+
+    # y_m = np.array([0.924, 0.947, 0.961, 0.965, 0.9730, 0.976, 0.9778])
+    # y_fm = np.array([0.681, 0.874, 0.943, 0.9575, 0.968, 0.974, 0.9761])
+    # plot_lr_result(n, y_m, y_fm)
+
+    # y_m = np.array([0.911, 0.9422, 0.9562, 0.958, 0.9646, 0.9692, 0.9734])
+    # y_fm = np.array([0.637, 0.850, 0.9315, 0.9404, 0.9626, 0.9654, 0.9754])
+    # plot_lr_result(n, y_m, y_fm)
+
+    y_m = np.array([0.934, 0.9410, 0.944, 0.9521, 0.9655, 0.9787, 0.9834])
+    y_fm = np.array([0.887, 0.914, 0.9354, 0.9387, 0.941, 0.9547, 0.9654])
     plot_lr_result(n, y_m, y_fm)
