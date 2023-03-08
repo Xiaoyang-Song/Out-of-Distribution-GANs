7ff13d3e8a1840ced9d7f6aa4e1f22d219f8192c
xysong <xysong25@gmail.com>
2023-03-08 13:41:38 -0500

    GL: bash file done

--- gl.py
+++ gl.py
@@ -1,3 +1,6 @@
 import torch
+from icecream import ic
+
+ic("icecream installed")
 print("hello gl")
 print(torch.cuda().is_available())
---
+++ gl.sh
@@ -0,0 +1,16 @@
+#SBATCH --account=sunwbgt0
+#SBATCH --job-name=test
+#SBACH --mail-user=xysong@umich.edu
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=10000m
+#SBATCH --time=1:00:00
+#SBATCH --partition=gpu
+
+module purge
+pip3 install --user icecream
+pip3 install --user torch
+
+
+python3 gl.py
