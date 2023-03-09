b02dd20844bf2cf9be4312a8e5c1acdfe3054b39
xysong <xysong25@gmail.com>
2023-03-09 17:44:40 -0500

    Update: ood gan code sync

--- ood_gan.py
+++ ood_gan.py
@@ -9,6 +9,9 @@ from metrics import *
 from wasserstein import *
 from torch.utils.tensorboard import SummaryWriter
 from time import gmtime, strftime
+from sklearn.decomposition import PCA
+
+from zmq.backend import device
 
 
 def ood_gan_d_loss(logits_real, logits_fake, logits_ood, labels_real):
@@ -25,15 +28,61 @@ def ood_gan_d_loss(logits_real, logits_fake, logits_ood, labels_real):
 
 
 def ood_gan_g_loss(logits_fake, img_fake=None, img_ind=None,
-                   img_ood=None, ind_fake_sample_size=64):
+                   img_ood=None, ind_fake_sample_size=32, model=None):
     # 1. Wasserstein distance of G(z)
     w_fake = batch_wasserstein(logits_fake)
     # 2. Distance between X_in and G(z)
-    dist_fake_ind = get_dist_metric(
-        img_fake, img_ind, ind_fake_sample_size, DIST_TYPE.EUC)
+    # dist_fake_ind = get_dist_metric(
+    #     img_fake, img_ind, ind_fake_sample_size, DIST_TYPE.EUC)
+
+    ######################
+    # Euclidean Distance #
+    ######################
+    # dist_fake_ind = torch.sqrt(torch.sum((xin - gz)**2))
+
+    ###############
+    # Correlation #
+    ###############
+    # xin = torch.mean(img_ind, dim=0).reshape(1, -1)  # 1 x HW
+    # gz = torch.mean(img_fake, dim=0).reshape(1, -1)  # 1 x HW
+    # target_mat = torch.cat([xin, gz])
+    # dist_fake_ind = -torch.corrcoef(target_mat)[0][1]
+
+    ########################
+    # Feature Vector Angle #
+    ########################
+    target_feat = model(img_ood).mean(dim=0).squeeze()
+    gz_feat = model(img_fake).mean(dim=0).squeeze()
+    # ic(target_feat)
+    # ic(gz_feat)
+
+    norm1, norm2 = LA.norm(target_feat), LA.norm(gz_feat)
+    dist_fake_ood = torch.dot(target_feat, gz_feat) / (norm1 * norm2)
+
     # 3. Distance between X_ood and G(z)
-    dist_fake_ood = None
-    return w_fake, dist_fake_ind, dist_fake_ood
+    #######
+    # PCA #
+    #######
+    # cluster_center = img_ood.flatten(1,3)
+    # pca = PCA(n_components=16)
+    # pca.fit(cluster_center.cpu())
+    # # print(pca.explained_variance_ratio_)
+    # # ic(sum(pca.explained_variance_ratio_))
+    # pca_feat = pca.transform(img_ood.flatten(1,3).cpu())
+    # target_feat = torch.tensor(pca_feat.mean(axis=0)).to(DEVICE)
+    # # gz
+    # pca_gz = PCA(n_components=16)
+    # pca_gz.fit(img_fake.flatten(1,3).cpu())
+    # pca_feat = pca.transform(img_fake.flatten(1,3).cpu())
+    # # ic(sum(pca_gz.explained_variance_ratio_))
+    # gz_feat = torch.tensor(pca_feat.mean(axis=0)).to(DEVICE)
+
+    # norm1, norm2 = LA.norm(target_feat), LA.norm(gz_feat)
+    # dist_fake_ood = torch.dot(target_feat, gz_feat) / (norm1 * norm2)
+
+    # dist_fake_ood = None
+    dist_fake_ind = None
+    return w_fake, dist_fake_ind, -dist_fake_ood
 
 
 class OOD_GAN_TRAINER():
@@ -63,7 +112,7 @@ class OOD_GAN_TRAINER():
         self.max_epochs = max_epochs
         self.hp = hp
 
-    def train(self, ind_loader, ood_img_batch, D_solver, G_solver, pretrainedD=None, checkpoint=None):
+    def train(self, ind_loader, ood_img_batch, xb, D_solver, G_solver, metric=None, pretrainedD=None, checkpoint=None):
         # Load pretrained Discriminator
         if pretrainedD is not None:
             pretrain = torch.load(pretrainedD)
@@ -79,12 +128,19 @@ class OOD_GAN_TRAINER():
         # Print out OoD sample statistics
         ic(f"OoD sample shape: {ood_img_batch.shape}")
         ood_img_batch = ood_img_batch.to(DEVICE)
+        # for key in xb:
+        #   xb[key] = xb[key].to(DEVICE)
         # Training loop
         iter_count = 0
         for epoch in range(self.max_epochs):
             for steps, (x, y) in enumerate(ind_loader):
+                cls = int(iter_count % 5)
+                # cls=0
                 x = x.to(DEVICE)
                 y = y.to(DEVICE)
+                # ic(x.shape)
+                # ic(x.mean(dim=(1,2,3)).unsqueeze(-1).shape)
+                # x = (x - x.mean(dim=(1,2,3)).unsqueeze(-1)) / x.std(dim=(1,2,3)).unsqueeze(-1)
                 # Manually discard last batch
                 if len(x) != self.bsz_tri:
                     continue
@@ -96,11 +152,14 @@ class OOD_GAN_TRAINER():
                 logits_real = self.D(x)
                 # Logits for G(z)
                 seed = torch.rand(
-                    (self.bsz_tri, self.noise_dim, 1, 1), device=DEVICE) * 2 - 1
-                Gz = self.G(seed).detach()
+                    (self.bsz_tri, self.noise_dim), device=DEVICE) * 2 - 1
+                Gz = self.G(seed, [cls]*self.bsz_tri).detach()
                 logits_fake = self.D(Gz)
                 # Logits for X_ood
-                logits_ood = self.D(ood_img_batch)
+                ood_idx = np.random.choice(len(ood_img_batch), min(
+                    len(ood_img_batch), 10), replace=False)
+                ood_img = ood_img_batch[ood_idx, :, :, :]
+                logits_ood = self.D(ood_img)
                 # Compute loss
                 ind_ce_loss, w_ood, w_fake = self.dloss(
                     logits_real, logits_fake, logits_ood, y)
@@ -127,22 +186,26 @@ class OOD_GAN_TRAINER():
                     G_solver.zero_grad()
                     # Logits for G(z)
                     seed = torch.rand(
-                        (self.bsz_tri, self.noise_dim, 1, 1), device=DEVICE) * 2 - 1
-                    Gz = self.G(seed).to(DEVICE).detach()
+                        (self.bsz_tri, self.noise_dim), device=DEVICE) * 2 - 1
+                    Gz = self.G(seed,  [cls]*self.bsz_tri).to(DEVICE).detach()
                     logits_fake = self.D(Gz)
                     # Compute loss
+                    # Minimize ood and gz
                     w_z, dist_fake_ind, dist_fake_ood = self.gloss(
-                        logits_fake, Gz, x, ood_img_batch)
+                        logits_fake, Gz, None, ood_img_batch, model=metric)
                     g_total = -(self.hp.wass * (w_z) -
-                                self.hp.dist * dist_fake_ind)
+                                self.hp.dist * dist_fake_ood)
 
                     # Write statistics
                     global_step = steps*self.gd_steps_ratio+g_step
                     self.writer.add_scalars("Generator Loss/each", {
                         'W_z': w_z.detach(),
-                        'd_ind': dist_fake_ind.detach(),
-                        # 'd_ood': dist_fake_ood.detach()
+                        # 'd_ind': -dist_fake_ind.detach(),
+                        # 'd_ood': -dist_fake_ind.detach(),
+                        'd_ood': -dist_fake_ood.detach()
                     }, global_step)
+                    self.writer.add_scalar(
+                        f"Generator Loss/distance/{cls}", -dist_fake_ood.detach(), global_step // 5)
                     self.writer.add_scalar(
                         "Generator Loss/total", g_total.detach(), global_step)
 
@@ -152,17 +215,17 @@ class OOD_GAN_TRAINER():
 
                 # Print out statistics
                 if (iter_count % self.n_steps_log == 0):
+                    # print(
+                    #     f"Step: {steps:<4} | {cls} | D: {d_total.item(): .4f} | CE: {ind_ce_loss.item(): .4f} | W_OoD: {-torch.log(-w_ood).item(): .4f} | W_z: {-torch.log(-w_fake).item(): .4f} | G: {g_total.item(): .4f} | d_ind: {dist_fake_ind.item(): .4f} | d_pixel: {((dist_fake_ind.item()**2)/784)**0.5: .4f} | W_z: {-torch.log(-w_z).item(): .4f}")
                     print(
-                        f"Step: {steps:<4} |\
-                            D: {d_total.item(): .4f} | CE: {ind_ce_loss.item(): .4f} | W_OoD: {w_ood.item(): .4f} | W_z: {w_fake.item(): .4f} |\
-                            G: {g_total.item(): .4f} | d_ind: {dist_fake_ind.item(): .4f} | W_z: {w_z.item(): .4f}")
+                        f"Step: {steps:<4} | {cls} | D: {d_total.item(): .4f} | CE: {ind_ce_loss.item(): .4f} | W_OoD: {-torch.log(-w_ood).item(): .4f} | W_z: {-torch.log(-w_fake).item(): .4f} | G: {g_total.item(): .4f} | CosSim: {-dist_fake_ood.item(): .4f} | W_z: {-torch.log(-w_z).item(): .4f}")
                 iter_count += 1
 
             # Save checkpoint
             ckpt_name = f"{self.ckpt_dir}{self.ckpt_name}_[{epoch}].pt"
             torch.save({
                 'D-state': self.D.state_dict(),
-                'G-state': self.G.state_dict(),
-                'writer': self.writer
+                'G-state': self.G.state_dict()
+                # 'writer': self.writer
             }, ckpt_name)
             print(f'New checkpoint created at the end of epoch {epoch}.')
