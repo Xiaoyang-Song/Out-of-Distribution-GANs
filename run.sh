3c54ff3567a6414746820fc7422b76c3fc4f6505
xysong <xysong25@gmail.com>
2023-03-11 20:32:00 -0500

    Sync .sh

--- run.sh
+++ run.sh
@@ -3,12 +3,16 @@
 #SBATCH --account=sunwbgt0
 #SBATCH --job-name=test
 #SBATCH --mail-user=xysong@umich.edu
-#SBATCH --mail-type=END
+#SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=1
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:1
 #SBATCH --mem-per-gpu=1000m
 #SBATCH --time=1:00:00
-#SBATCH --partition=gpu
+#SBATCH --cpus-per-gpu=1
+
+module purge
+pip3 install torch
+pip3 install icecream
 
 python3 gl.py
