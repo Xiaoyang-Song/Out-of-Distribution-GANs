276c9b7a87a2a9a55230d66ec9dbaff0b5c6f780
xysong <xysong25@gmail.com>
2023-03-22 23:56:42 -0400

    Fix: bugs

--- main/main_ood.py
+++ main/main_ood.py
@@ -10,7 +10,7 @@ import time
 parser = argparse.ArgumentParser()
 parser.add_argument('--mc', help='Number of MC', type=int)
 parser.add_argument('--num_epochs', help='Number of Epochs', type=int)
-parser.add_argument('--balanced', help='Balanced', type=bool, default=True)
+parser.add_argument('--balanced', help='Balanced', type=bool)
 parser.add_argument('--n_ood', help='Number of observed OoD', type=int)
 args = parser.parse_args()
 ic(args.balanced)
