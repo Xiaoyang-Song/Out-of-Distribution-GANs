02a5d9202b579266c3035f751c7890d0f2428c2d
xysong <xysong25@gmail.com>
2023-03-26 01:31:18 -0400

    Update: main.sh

--- main/main.sh
+++ main/main.sh
@@ -14,4 +14,4 @@ module purge
 conda init bash
 conda activate OoD
 
-python3 main_ood.py --mc=3 --num_epochs=1 --balanced=imbalance --n_ood=3
+python3 main_ood.py --mc=5 --num_epochs=1 --balanced=balance --n_ood=2
