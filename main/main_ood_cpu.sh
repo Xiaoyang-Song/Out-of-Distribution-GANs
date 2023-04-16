c4c0a91c448bf0e435bfe034fdb171729629cfde
xysong <xysong25@gmail.com>
2023-04-16 01:40:19 -0400

    Update: cpu training

---
+++ main/main_ood_cpu.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#SBATCH --account=sunwbgt0
+#SBATCH --job-name=OoD-training
+#SBATCH --mail-user=xysong@umich.edu
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --nodes=1
+#SBATCH --partition=standard
+#SBATCH --time=24:00:00
+
+module purge
+conda init bash
+conda activate OoD
+
+# python3 main_ood.py --config=../config/GAN/OOD-GAN-FashionMNIST.yaml
+python3 main_ood.py --config=../config/GAN/OOD-GAN-FashionMNIST-MNIST.yaml
+# python3 main_ood.py --config=../config/GAN/OOD-GAN-CIFAR10-SVHN.yaml
