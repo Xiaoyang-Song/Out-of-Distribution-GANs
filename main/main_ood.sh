77c0f38845245bfda96effc36ea9172545dc2f80
xysong <xysong25@gmail.com>
2023-04-24 19:36:21 -0400

    Update: SVHN

--- main/main_ood.sh
+++ main/main_ood.sh
@@ -14,6 +14,7 @@ module purge
 conda init bash
 conda activate OoD
 
+# python3 main_ood.py --config=../config/GAN/OOD-GAN-MNIST.yaml
 # python3 main_ood.py --config=../config/GAN/OOD-GAN-FashionMNIST.yaml
 # python3 main_ood.py --config=../config/GAN/OOD-GAN-FashionMNIST-MNIST.yaml
 # python3 main_ood.py --config=../config/GAN/OOD-GAN-CIFAR10-SVHN.yaml
