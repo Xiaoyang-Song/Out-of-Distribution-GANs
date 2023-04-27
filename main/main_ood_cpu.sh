f872db56bc9bbdbb8c2522511b462afe00432dd7
xysong <xysong25@gmail.com>
2023-04-27 18:07:27 -0400

    Update: cpu training

--- main/main_ood_cpu.sh
+++ main/main_ood_cpu.sh
@@ -6,6 +6,8 @@
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --nodes=1
 #SBATCH --partition=standard
+#SBATCH --mem=16GB
+#SBATCH --cpus-per-task=10
 #SBATCH --time=24:00:00
 
 module purge
@@ -13,5 +15,5 @@ conda init bash
 conda activate OoD
 
 # python3 main_ood.py --config=../config/GAN/OOD-GAN-FashionMNIST.yaml
-python3 main_ood.py --config=../config/GAN/OOD-GAN-FashionMNIST-MNIST.yaml
-# python3 main_ood.py --config=../config/GAN/OOD-GAN-CIFAR10-SVHN.yaml
+# python3 main_ood.py --config=../config/GAN/OOD-GAN-FashionMNIST-MNIST.yaml
+python3 main_ood.py --config=../config/GAN/OOD-GAN-CIFAR10-SVHN.yaml
