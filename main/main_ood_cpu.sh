9e8805b17d628e368353e71a580ff38f4a0102ce
xysong <xysong25@gmail.com>
2023-04-28 02:21:22 -0400

    Launch B = 128

--- main/main_ood_cpu.sh
+++ main/main_ood_cpu.sh
@@ -16,4 +16,5 @@ conda activate OoD
 
 # python3 main_ood.py --config=../config/GAN/OOD-GAN-FashionMNIST.yaml
 # python3 main_ood.py --config=../config/GAN/OOD-GAN-FashionMNIST-MNIST.yaml
-python3 main_ood.py --config=../config/GAN/OOD-GAN-CIFAR10-SVHN.yaml
+# python3 main_ood.py --config=../config/GAN/OOD-GAN-CIFAR10-SVHN.yaml
+python3 main_ood.py --config=../config/GAN/OOD-GAN-SVHN.yaml
