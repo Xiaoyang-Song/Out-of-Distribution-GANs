3bde1ce7f0dd8c3e75e5400897b4b578de799c27
xysong <xysong25@gmail.com>
2023-04-28 00:32:58 -0400

    SVHN pretraining

--- main/classifier_training.py
+++ main/classifier_training.py
@@ -9,15 +9,18 @@ import yaml
 
 log_dir = "checkpoint/pretrained/"
 os.makedirs(log_dir, exist_ok=True)
-# model = DenseNet3(depth=100, num_classes=10, input_channel=3).to(DEVICE)
-img_info = {'H': 28, 'W': 28, 'C': 1}
+model = DenseNet3(depth=100, num_classes=8, input_channel=3).to(DEVICE)
+# img_info = {'H': 28, 'W': 28, 'C': 1}
 model = DC_D(5, img_info).to(DEVICE)
 
 lr = 1e-3
 optimizer = torch.optim.Adam(model.parameters(), lr=lr, betas=(0.9, 0.999))
 # CIFAR10-SVHN
-# dset = DSET("CIFAR10-SVHN", False, 50, 64, None, None)
-dset = DSET("MNIST", True, 50, 256, [2, 3, 6, 8, 9], [1, 7])
+dset = DSET("CIFAR10-SVHN", False, 50, 64, None, None)
+# MNIST
+# dset = DSET("MNIST", True, 50, 256, [2, 3, 6, 8, 9], [1, 7])
+# SVHN
+dset = DSET("SVHN", True, 50, 64, [0, 1, 2, 3, 4, 5, 6, 7], [8, 9])
 
 
 ind_tri_loader = dset.ind_train_loader
