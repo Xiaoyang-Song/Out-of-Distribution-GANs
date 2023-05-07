1d1a6883ae7c155b115cf9601ef505efbcdeff9e
xysong <xysong25@gmail.com>
2023-05-07 11:07:34 -0400

    Update: config

---
+++ main/sample_ood.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#SBATCH --account=alkontar1
+#SBATCH --job-name=OoD-training
+#SBATCH --mail-user=xysong@umich.edu
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --nodes=1
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:1
+#SBATCH --mem-per-gpu=14GB
+#SBATCH --time=36:00:00
+
+module purge
+conda init bash
+conda activate OoD
+
+# python3 main_wood.py --config=../config/WOOD/WOOD-FashionMNIST.yaml
+python3 sample_ood.py --config=../config/sample.yaml
