3c8e2e8b9c858e75510ba4dba9fc84870c3ad5a9
xysong <xysong25@gmail.com>
2023-06-16 16:21:38 -0400

    Update: plots

--- jobs/cifar10-svhn/ood-gan/8.sh
+++ jobs/cifar10-svhn/ood-gan/8.sh
@@ -13,4 +13,5 @@
 module purge
 conda init bash
 
-conda activate OoDpython3 main/main_ood.py --config=config/GAN/OOD-GAN-CIFAR10-SVHN.yaml --n_ood=8 > checkpoint/log/CIFAR10-SVHN/OOD-GAN/log-8.txt
+conda activate OoD
+python3 main/main_ood.py --config=config/GAN/OOD-GAN-CIFAR10-SVHN.yaml --n_ood=8 > checkpoint/log/CIFAR10-SVHN/OOD-GAN/log-8.txt
