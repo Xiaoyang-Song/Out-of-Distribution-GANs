758d58fb1a7f1a6ae3b232a10a0ae5a15c2356b5
xysong <xysong25@gmail.com>
2023-06-27 23:07:08 -0400

    Update: sbatch jobs

--- jobs/simulation/simulation.sh
+++ jobs/simulation/simulation.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 #SBATCH --account=sunwbgt0
 #SBATCH --job-name=OoD-SIM
 #SBATCH --mail-user=xysong@umich.edu
@@ -22,6 +21,9 @@ python3 simulation.py --mode=G --config=config/simulation/setting_1_config.yaml
 python3 simulation.py --mode=R --config=config/simulation/run_config.yaml --n_ood=2 --h=128 --beta=1 --w_ce=1 --w_ood=1 --w_z=1 --wood_lr=0.001 --gan_lr=0.0001 --bsz_tri=256 --bsz_val=256 --bsz_ood=2 --n_d=1 --n_g=1
 python3 simulation.py --mode=R --config=config/simulation/run_config.yaml --n_ood=2 --h=128 --beta=1 --w_ce=1 --w_ood=1 --w_z=1 --wood_lr=0.001 --gan_lr=0.0001 --bsz_tri=256 --bsz_val=256 --bsz_ood=2 --n_d=2 --n_g=1
 python3 simulation.py --mode=R --config=config/simulation/run_config.yaml --n_ood=2 --h=128 --beta=1 --w_ce=1 --w_ood=1 --w_z=1 --wood_lr=0.001 --gan_lr=0.0001 --bsz_tri=256 --bsz_val=256 --bsz_ood=2 --n_d=3 --n_g=1
+python3 simulation.py --mode=R --config=config/simulation/run_config.yaml --n_ood=2 --h=128 --beta=1 --w_ce=1 --w_ood=1 --w_z=0.1 --wood_lr=0.001 --gan_lr=0.0001 --bsz_tri=256 --bsz_val=256 --bsz_ood=2 --n_d=2 --n_g=1
+python3 simulation.py --mode=R --config=config/simulation/run_config.yaml --n_ood=2 --h=128 --beta=1 --w_ce=1 --w_ood=1 --w_z=0.01 --wood_lr=0.001 --gan_lr=0.0001 --bsz_tri=256 --bsz_val=256 --bsz_ood=2 --n_d=2 --n_g=1
+python3 simulation.py --mode=R --config=config/simulation/run_config.yaml --n_ood=2 --h=128 --beta=1 --w_ce=1 --w_ood=1 --w_z=0.001 --wood_lr=0.001 --gan_lr=0.0001 --bsz_tri=256 --bsz_val=256 --bsz_ood=2 --n_d=2 --n_g=1
 
 
 
