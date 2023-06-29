7af5b944cb2e457f6a1a02ce777fdf37e94615b6
xysong <xysong25@gmail.com>
2023-06-28 21:56:26 -0400

    Update: jobs

--- jobs/simulation/parallel_jobs/job11.sh
+++ jobs/simulation/parallel_jobs/job11.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --account=alkontar1
+#SBATCH --account=jhjin1
 #SBATCH --job-name=J11
 #SBATCH --mail-user=xysong@umich.edu
 #SBATCH --mail-type=BEGIN,END,FAIL
--- jobs/simulation/parallel_jobs/job8.sh
+++ jobs/simulation/parallel_jobs/job8.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --account=alkontar1
+#SBATCH --account=sunwbgt0
 #SBATCH --job-name=J8
 #SBATCH --mail-user=xysong@umich.edu
 #SBATCH --mail-type=BEGIN,END,FAIL
