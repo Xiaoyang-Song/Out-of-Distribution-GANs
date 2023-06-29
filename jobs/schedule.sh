48d99587dd35060d702f1dff220f89fc62236209
xysong <xysong25@gmail.com>
2023-06-28 23:01:39 -0400

    Fix: bugs

--- jobs/schedule.sh
+++ jobs/schedule.sh
@@ -3,14 +3,14 @@
 # scheduling
 
 # FashionMNIST WOOD
-# sbatch jobs/simulation/fashionmnist/wood/4.sh
-# sbatch jobs/simulation/fashionmnist/wood/8.sh
-# sbatch jobs/simulation/fashionmnist/wood/16.sh
-# sbatch jobs/simulation/fashionmnist/wood/32.sh
-# sbatch jobs/simulation/fashionmnist/wood/64.sh
-# sbatch jobs/simulation/fashionmnist/wood/128.sh
-# sbatch jobs/simulation/fashionmnist/wood/256.sh
-sbatch jobs/simulation/fashionmnist/wood/512.sh
-sbatch jobs/simulation/fashionmnist/wood/1024.sh
-sbatch jobs/simulation/fashionmnist/wood/2048.sh
-sbatch jobs/simulation/fashionmnist/wood/4096.sh
+# sbatch jobs/fashionmnist/wood/4.sh
+# sbatch jobs/fashionmnist/wood/8.sh
+# sbatch jobs/fashionmnist/wood/16.sh
+# sbatch jobs/fashionmnist/wood/32.sh
+# sbatch jobs/fashionmnist/wood/64.sh
+# sbatch jobs/fashionmnist/wood/128.sh
+# sbatch jobs/fashionmnist/wood/256.sh
+sbatch jobs/fashionmnist/wood/512.sh
+sbatch jobs/fashionmnist/wood/1024.sh
+sbatch jobs/fashionmnist/wood/2048.sh
+sbatch jobs/fashionmnist/wood/4096.sh
