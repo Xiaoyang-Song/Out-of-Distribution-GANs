dd8570a0a531d2cce739f174a15dd615b4dbc35b
xysong <xysong25@gmail.com>
2023-07-01 21:01:24 -0400

    Update: cancel jobs

--- jobs/cancel.sh
+++ jobs/cancel.sh
@@ -1,4 +1,31 @@
 #!/bin/bash
 
 # Cancel jobs
-# scancel 55148133
+scancel 55199286
+scancel 55199287
+scancel 55199288
+scancel 55199289
+scancel 55199290
+scancel 55199291
+scancel 55199292
+scancel 55199293
+scancel 55199294
+scancel 55199295
+scancel 55199296
+scancel 55199297
+scancel 55199298
+scancel 55199299
+scancel 55199300
+scancel 55199301
+scancel 55199302
+scancel 55199303
+scancel 55199304
+scancel 55199305
+scancel 55199306
+scancel 55199307
+scancel 55199308
+scancel 55199309
+scancel 55199310
+scancel 55199311
+scancel 55199312
+
