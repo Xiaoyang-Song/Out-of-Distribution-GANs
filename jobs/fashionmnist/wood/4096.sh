2bc4c3e9085a568d21d723d3c70cd674ce725df5
xysong <xysong25@gmail.com>
2023-07-01 21:44:05 -0400

    Update: fashionmnist validation

--- jobs/fashionmnist/wood/4096.sh
+++ jobs/fashionmnist/wood/4096.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --account=jhjin1
+#SBATCH --account=sunwbgt0
 #SBATCH --job-name=WDFM4096
 #SBATCH --mail-user=xysong@umich.edu
 #SBATCH --mail-type=BEGIN,END,FAIL
@@ -9,10 +9,10 @@
 #SBATCH --gres=gpu:1
 #SBATCH --mem-per-gpu=16GB
 #SBATCH --time=144:00:00
-#SBATCH --output=/home/xysong/Out-of-Distribution-GANs/slurm-jobs/WDFM4096.log
+#SBATCH --output=/home/xysong/Out-of-Distribution-GANs/slurm-jobs/WDFM4096-50-10.log
 
 module purge
 conda init bash
 conda activate OoD
 
-python3 main/main_wood.py --config=config/WOOD/WOOD-FashionMNIST.yaml --n_ood=4096 > checkpoint/log/FashionMNIST/WOOD/log-4096.txt
+python3 main/main_wood.py --config=config/WOOD/WOOD-FashionMNIST.yaml --n_ood=4096 > checkpoint/log/FashionMNIST/WOOD/log-4096-50-10.txt
