997e26e3f073715723df59e27f3e79ffa3fff4e5
xysong <xysong25@gmail.com>
2023-08-15 21:12:52 -0400

    Update: new jobs

---
+++ jobs/cifar10-svhn-r2/ood-gan/256.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#SBATCH --account=sunwbgt98
+#SBATCH --job-name=CSGAN256
+#SBATCH --mail-user=xysong@umich.edu
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --nodes=1
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:1
+#SBATCH --mem-per-gpu=16GB
+#SBATCH --time=144:00:00
+#SBATCH --output=/home/xysong/Out-of-Distribution-GANs/slurm-jobs/CSGAN256-R2.log
+
+module purge
+conda init bash
+conda activate OoD
+python3 main/main_ood.py --config=config/GAN/OOD-GAN-CIFAR10-SVHN-R2.yaml --n_ood=256 > checkpoint/log/CIFAR10-SVHN-R2/OOD-GAN/log-256.txt
