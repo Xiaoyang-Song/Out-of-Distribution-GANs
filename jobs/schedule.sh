84c1e1f0f9a64d58d5151c70f1d29be3488fd5e2
xysong <xysong25@gmail.com>
2023-09-01 20:52:54 -0400

    Update: new validation jobs

--- jobs/schedule.sh
+++ jobs/schedule.sh
@@ -51,16 +51,16 @@ export PYTHONPATH=$PYTHONPATH$:`pwd`
 # sbatch jobs/fashionmnist/ood-gan/4096.sh
 
 # MNIST OoD GAN
-# sbatch jobs/mnist/ood-gan/4.sh
-# sbatch jobs/mnist/ood-gan/8.sh
-# sbatch jobs/mnist/ood-gan/16.sh
-# sbatch jobs/mnist/ood-gan/32.sh
+sbatch jobs/mnist/ood-gan/4.sh
+sbatch jobs/mnist/ood-gan/8.sh
+sbatch jobs/mnist/ood-gan/16.sh
+sbatch jobs/mnist/ood-gan/32.sh
 # sbatch jobs/mnist/ood-gan/64.sh
 # sbatch jobs/mnist/ood-gan/128.sh
 # sbatch jobs/mnist/ood-gan/256.sh
 # sbatch jobs/mnist/ood-gan/512.sh
-# sbatch jobs/mnist/ood-gan/1024.sh
-# sbatch jobs/mnist/ood-gan/2048.sh
+sbatch jobs/mnist/ood-gan/1024.sh
+sbatch jobs/mnist/ood-gan/2048.sh
 # sbatch jobs/mnist/ood-gan/4096.sh
 
 # MNIST-FashionMNIST OoD GAN
@@ -91,8 +91,8 @@ export PYTHONPATH=$PYTHONPATH$:`pwd`
 
 
 # SVHN OoD GAN
-sbatch jobs/svhn/ood-gan/svhn-4.sh
-sbatch jobs/svhn/ood-gan/svhn-8.sh
+# sbatch jobs/svhn/ood-gan/svhn-4.sh
+# sbatch jobs/svhn/ood-gan/svhn-8.sh
 # sbatch jobs/svhn/ood-gan/svhn-16.sh
 # sbatch jobs/svhn/ood-gan/svhn-32.sh
 # sbatch jobs/svhn/ood-gan/svhn-64.sh
