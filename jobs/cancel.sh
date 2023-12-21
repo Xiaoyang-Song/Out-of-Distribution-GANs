af39b16a64ab7d3e7cd0e826e738018a03ec2836
Xiaoyang-Song <xysong@umich.edu>
2023-12-21 04:37:31 -0500

    Update: new setups

--- jobs/cancel.sh
+++ jobs/cancel.sh
@@ -1,31 +1,12 @@
 #!/bin/bash
 
 # Cancel jobs
-scancel 55209792
-scancel 55209793
-scancel 55209794
-scancel 55209795
-scancel 55209796
-scancel 55209797
-scancel 55209798
-scancel 55209799
-scancel 55209800
-scancel 55209801
-scancel 55209802
-scancel 55209803
-scancel 55209804
-scancel 55209805
-scancel 55209806
-scancel 55209807
-scancel 55209808
-scancel 55209809
-scancel 55209810
-scancel 55209811
-scancel 55209812
-scancel 55209813
-scancel 55209814
-scancel 55209815
-scancel 55209816
-scancel 55209817
+scancel 47569
+scancel 47570
+scancel 47571
+scancel 47572
+scancel 47573
+scancel 47574
+scancel 47575
 
 
