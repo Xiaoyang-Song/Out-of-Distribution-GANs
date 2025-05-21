81e07ded3e0595032595dee826efe185d7ffb996
Xiaoyang-Song <xysong@umich.edu>
2025-05-21 15:11:39 -0400

    Sync

--- jobs/fashionmnist/ood-gan/8.sh
+++ jobs/fashionmnist/ood-gan/8.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 
-#SBATCH --account=alkontar1
+#SBATCH --account=sunwbgt0
 #SBATCH --job-name=GANFM8
 #SBATCH --mail-user=xysong@umich.edu
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --nodes=1
 #SBATCH --partition=gpu
-#SBATCH --gres=gpu:1
+#SBATCH --gpus=1
 #SBATCH --mem-per-gpu=16GB
-#SBATCH --time=144:00:00
-#SBATCH --output=/home/xysong/Out-of-Distribution-GANs/slurm-jobs/GANFM8.log
-
-module purge
-conda init bash
-conda activate OoD
+#SBATCH --time=8:00:00
+#SBATCH --output=/scratch/sunwbgt_root/sunwbgt98/xysong/Out-of-Distribution-GANs/checkpoint/out/fm-8.log
 
 python3 main/main_ood.py --config=config/GAN/OOD-GAN-FashionMNIST.yaml --n_ood=8 > checkpoint/log/FashionMNIST/OOD-GAN/log-8.txt
--- jobs/schedule.sh
+++ jobs/schedule.sh
@@ -25,14 +25,14 @@ export PYTHONPATH=$PYTHONPATH$:`pwd`
 
 # FashionMNIST OoD GAN
 sbatch jobs/fashionmnist/ood-gan/4.sh
-# sbatch jobs/fashionmnist/ood-gan/8.sh
+sbatch jobs/fashionmnist/ood-gan/8.sh
 # sbatch jobs/fashionmnist/ood-gan/16.sh
 # sbatch jobs/fashionmnist/ood-gan/32.sh
-# sbatch jobs/fashionmnist/ood-gan/64.sh
+sbatch jobs/fashionmnist/ood-gan/64.sh
 # sbatch jobs/fashionmnist/ood-gan/128.sh
 # sbatch jobs/fashionmnist/ood-gan/256.sh
 # sbatch jobs/fashionmnist/ood-gan/512.sh
-# sbatch jobs/fashionmnist/ood-gan/1024.sh
+sbatch jobs/fashionmnist/ood-gan/1024.sh
 # sbatch jobs/fashionmnist/ood-gan/2048.sh
 # sbatch jobs/fashionmnist/ood-gan/4096.sh
 
